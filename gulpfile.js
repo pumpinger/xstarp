559c35f529c0842bed76babc05ceacac08047dbf
van <wzj92712@163.com>
2016-02-22 14:42:05 +0800

    1.0 spriter

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+/**
+ * Created by van on 2016/2/20.
+ */
+
+
+var gulp = require('gulp'),
+
+    minifyCSS = require('gulp-minify-css'),
+    spriter = require('gulp-css-spriter');
+
+gulp.task('spriter', function () {
+
+    var timestamp = +new Date();
+    //需要自动合并雪碧图的样式文件
+    return gulp.src('./src/css/spriter.css')
+        .pipe(spriter({
+            // 生成的spriter的位置
+            'spriteSheet': './img/sprite.png',
+            // 生成样式文件图片引用地址的路径
+            // 如下将生产：backgound:url(../images/sprite20324232.png)
+            'pathToSpriteSheetFromCSS': '../img/sprite.png'
+        }))
+        //.pipe(minifyCSS())
+        //产出路径
+        .pipe(gulp.dest('./css'));
+});
+
