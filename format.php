e8a8d17dea0f65ae613160a21e636748d5f8403e
van <wzj92712@163.com>
2016-12-21 10:33:34 +0800

    做 format

---
+++ format.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: @van
+ * Date: 2016/1/20
+ * Time: 15:49
+ */
+
+include "layout_header.php";
+
+?>
+
+
+
+
+
+
+
+
+<?php
+
+include "layout_footer.php";
+?>
