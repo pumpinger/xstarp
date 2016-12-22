4e71a883b7588ed933ea16551f309f2a538ce13d
gitjcc <jc_jesse@hotmail.com>
2016-12-22 18:43:39 +0800

    tree.php, ctrl-alt-l

--- tree.php
+++ tree.php
@@ -22,99 +22,6 @@ include "layout_header.php";
 <script type="text/javascript" src="src/js/tree.js"></script>
 
 
-<div class="tree-wrap">
-    <input class="test_tree1" placeholder="">
-</div>
-<script>
-    var testTree1 = xTree({
-        dom: $('.test_tree1'),
-        only_child: false,
-        is_multi: true,
-        is_trigger: false,
-        node_merge: false,
-        has_search: false,
-        expand: 3,
-        data: [
-            {id: 1, name: '行政部', nodeId: 0, is_node: true, is_check: false},
-            {id: 3, name: '财务部', nodeId: 1, is_node: true, is_check: false},
-            {id: 8, name: '财务部2', nodeId: 3, is_node: true, is_check: false},
-            {id: 5, name: '李职员', nodeId: 8, is_node: false, is_check: false},
-            {id: 6, name: '孙职员', nodeId: 8, is_node: false, is_check: false},
-            {id: 2, name: '张部长', nodeId: 1, is_node: false, is_check: false},
-            {id: 4, name: '刘职员', nodeId: 3, is_node: false, is_check: false},
-            {id: 1, name: '王经理', nodeId: 0, is_node: false, is_check: false}
-        ],
-        //        choose:{
-        //            nodeId:[1],
-        //            id:[1]
-        //        },
-        onInit: function () {
-        },
-        onOpen: function () {
-        },
-        onBeforeOpen: function () {
-        },
-        onClose: function (hasChange) {
-            console.log(testTree1.getId());
-        },
-        onCheck: function (item, dom, childrenItem) {
-//            console.log(this);
-//            console.log(this.getId());
-//            console.log(testTree1);
-//            应该是  this.getId()比较规范  还是 testTree1.getId()比较规范   this可以做到 只暴漏该暴漏的方法
-        },
-        onCancel: function (item, dom, childrenItem) {
-        }
-    });
-</script>
-
-<div class="tree-wrap">
-    <input class="test_tree" placeholder="">
-</div>
-<script>
-    var testTree = xTree({
-        dom: $('.test_tree'),
-        only_child: false,
-        is_multi: true,
-        node_merge: false,
-        has_search: true,
-        expand: 3,
-        data: [
-            {id: 1, name: '行政部', nodeId: 0, is_node: true, is_check: false},
-            {id: 3, name: '财务部', nodeId: 1, is_node: true, is_check: false},
-            {id: 8, name: '财务部2', nodeId: 3, is_node: true, is_check: false},
-            {id: 5, name: '李职员', nodeId: 8, is_node: false, is_check: false},
-            {id: 6, name: '孙职员', nodeId: 8, is_node: false, is_check: false},
-            {id: 2, name: '张部长', nodeId: 1, is_node: false, is_check: false},
-            {id: 4, name: '刘职员', nodeId: 3, is_node: false, is_check: false},
-            {id: 1, name: '王经理', nodeId: 0, is_node: false, is_check: false}
-        ],
-        //        choose:{
-        //            nodeId:[1],
-        //            id:[1]
-        //        },
-        onInit: function () {
-        },
-        onOpen: function () {
-        },
-        onBeforeOpen: function () {
-        },
-        onClose: function (hasChange) {
-            console.log(testTree.getId());
-            console.log(testTree._makeSearch);
-        },
-        onCheck: function (item, dom, childrenItem) {
-//            console.log(this);
-//            console.log(this.getId());
-//            console.log(testTree);
-//            应该是  this.getId()比较规范  还是 testTree.getId()比较规范   this可以做到 只暴漏该暴漏的方法
-        },
-        onCancel: function (item, dom, childrenItem) {
-        }
-    });
-</script>
-
-
 <div class="x-panel">
     <div class="x-panel-title">树型选择器</div>
     <div class="x-panel-content">
@@ -210,11 +117,13 @@ include "layout_header.php";
 
         <p class="x-color-blue">效果:</p>
         <p>方法一</p>
-        <div class="member_tree" style="width: 200px;height: 300px;border: 1px solid #333;"></div>
+        <div class="tree-wrap">
+            <input class="test_tree1" placeholder="">
+        </div>
 
         <p>方法二</p>
-        <div>
-            <input class="member_tree2" placeholder="">
+        <div class="tree-wrap">
+            <input class="test_tree2" placeholder="">
         </div>
 
 
@@ -257,9 +166,90 @@ include "layout_header.php";
     </div>
 </div>
 
+<script>
+    var testTree1 = xTree({
+        dom: $('.test_tree1'),
+        only_child: false,
+        is_multi: true,
+        is_trigger: false,
+        node_merge: false,
+        has_search: false,
+        expand: 3,
+        data: [
+            {id: 1, name: '行政部', nodeId: 0, is_node: true, is_check: false},
+            {id: 3, name: '财务部', nodeId: 1, is_node: true, is_check: false},
+            {id: 8, name: '财务部2', nodeId: 3, is_node: true, is_check: false},
+            {id: 5, name: '李职员', nodeId: 8, is_node: false, is_check: false},
+            {id: 6, name: '孙职员', nodeId: 8, is_node: false, is_check: false},
+            {id: 2, name: '张部长', nodeId: 1, is_node: false, is_check: false},
+            {id: 4, name: '刘职员', nodeId: 3, is_node: false, is_check: false},
+            {id: 1, name: '王经理', nodeId: 0, is_node: false, is_check: false}
+        ],
+        //        choose:{
+        //            nodeId:[1],
+        //            id:[1]
+        //        },
+        onInit: function () {
+        },
+        onOpen: function () {
+        },
+        onBeforeOpen: function () {
+        },
+        onClose: function (hasChange) {
+            console.log(testTree1.getId());
+        },
+        onCheck: function (item, dom, childrenItem) {
+//            console.log(this);
+//            console.log(this.getId());
+//            console.log(testTree1);
+//            应该是  this.getId()比较规范  还是 testTree1.getId()比较规范   this可以做到 只暴漏该暴漏的方法
+        },
+        onCancel: function (item, dom, childrenItem) {
+        }
+    });
+</script>
 
 <script>
-
+    var testTree2 = xTree({
+        dom: $('.test_tree2'),
+        only_child: false,
+        is_multi: true,
+        node_merge: false,
+        has_search: true,
+        expand: 3,
+        data: [
+            {id: 1, name: '行政部', nodeId: 0, is_node: true, is_check: false},
+            {id: 3, name: '财务部', nodeId: 1, is_node: true, is_check: false},
+            {id: 8, name: '财务部2', nodeId: 3, is_node: true, is_check: false},
+            {id: 5, name: '李职员', nodeId: 8, is_node: false, is_check: false},
+            {id: 6, name: '孙职员', nodeId: 8, is_node: false, is_check: false},
+            {id: 2, name: '张部长', nodeId: 1, is_node: false, is_check: false},
+            {id: 4, name: '刘职员', nodeId: 3, is_node: false, is_check: false},
+            {id: 1, name: '王经理', nodeId: 0, is_node: false, is_check: false}
+        ],
+        //        choose:{
+        //            nodeId:[1],
+        //            id:[1]
+        //        },
+        onInit: function () {
+        },
+        onOpen: function () {
+        },
+        onBeforeOpen: function () {
+        },
+        onClose: function (hasChange) {
+            console.log(testTree.getId());
+            console.log(testTree._makeSearch);
+        },
+        onCheck: function (item, dom, childrenItem) {
+//            console.log(this);
+//            console.log(this.getId());
+//            console.log(testTree);
+//            应该是  this.getId()比较规范  还是 testTree.getId()比较规范   this可以做到 只暴漏该暴漏的方法
+        },
+        onCancel: function (item, dom, childrenItem) {
+        }
+    });
 </script>
 
 <?php
