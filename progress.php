62f54386a58cf4382152a7c06e9a48e0ffd3da98
gitjcc <jc_jesse@hotmail.com>
2016-12-30 18:00:36 +0800

    'typeof'

--- progress.php
+++ progress.php
@@ -12,20 +12,24 @@ include "layout_header.php";
 <div class="x-panel">
     <div class="x-panel-title">标签</div>
     <div class="x-panel-content">
-        <p>示例：</p>
+        <p class="x-color-blue">示例：</p>
         <pre><code class="language-html"><textarea>
             <script type="text/javascript" src="src/js/progress.js"></script>
         </textarea></code></pre>
 
-        <p>效果:</p>
+        <p class="x-color-blue">效果:</p>
+        <div class="">1、时间驱动型</div>
         <div id="x-progress-time-driven" >
-            <span class=""></span>
+        </div>
+
+        <div class="">2、数据驱动型</div>
+        <div id="x-progress-data-driven" >
         </div>
 
         <!--<div id="x-progress-data-driven">-->
         <!--    <span class=""></span>-->
         <!--</div>-->
-        <p>规则:</p>
+        <p class="x-color-blue">规则:</p>
     </div>
 </div>
 
@@ -33,16 +37,30 @@ include "layout_header.php";
 <script type="text/javascript" src="src/js/progress.js"></script>
 
 <script>
-    var opt = {
-        dom: $('#x-progress-time-driven'),
+    var opt1 = {
+        dom: '#x-progress-time-driven',
+        type: 1,
         style: {
-            begin:{},
-            end:{
-                width:'100%'
-            }
+            begin:'30%',
+            b:'60%',
+            c:'90%',
+            end:'100%'
         }
     };
-    xProgress(opt);
+    var xP1 = xProgress(opt1);
+
+    var opt2 = {
+        dom: '#x-progress-data-driven',
+        type: 2,
+        style: {
+            begin:'30%',
+            b:'60%',
+            c:'90%',
+            end:'100%'
+        }
+    };
+    var xP2 = xProgress(opt2);
+
 </script>
 
 <?php
--- src/js/progress.js
+++ src/js/progress.js
@@ -4,10 +4,21 @@
 
 ;(function (window, $) {
     var xProgress = function (option) {
-        console.log(option);
+
+        if(typeof option !== "object"){
+            console.log('option不是"object"', option);
+            return false;
+        }
         this.option = option;
-        this.dom = option.dom;
+
+        if(typeof this.option.dom !== "string"){
+            console.log('this.option.dom不是"string"', this.option.dom);
+            return false;
+        }
+        this.dom = $(this.option.dom);
+
         this._init();
+
         return this;
     };
 
