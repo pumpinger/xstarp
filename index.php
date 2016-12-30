461dae1dea0a44c641e82b9c723ed8838d984867
van <wzj92712@163.com>
2016-12-30 12:35:47 +0800

    该一点点方向

--- index.php
+++ index.php
@@ -11,21 +11,29 @@ include "layout_header.php";
 
 ?>
 
-<nav>
-    <ul>
-        <li>如何使用</li>
-        <li>功能列表</li>
-        <li>关于我们</li>
-    </ul>
-</nav>
 
 <article>
     <section>
-        解决问题:
-        如何模块化  如何更新项目   尽快搭建工程化    项目如何解决静态文件缓存
+        <p class="x-title">如何使用:</p>
+        把dest目录拖进去
     </section>
+    <br>
+
     <section>
-        技术内容:
+        <p class="x-title">常用功能:</p>
+        ...
+    </section>
+    <br>
+
+    <section>
+        <p class="x-title"> 关于我们:</p>
+        小步创想
+    </section>
+    <br>
+
+    <section>
+        <p class="x-title">待解决问题:</p>
+        基础方法方便开发  数据视图绑定   如何模块化  如何更新项目   解决静态文件缓存
     </section>
 </article>
 
