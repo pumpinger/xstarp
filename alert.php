e712aceed5837d4607a8723b84c09d42a9b8e70e
cherishjia <494976535@qq.com>
2017-01-03 10:23:16 +0800

    tip

--- alert.php
+++ alert.php
@@ -18,22 +18,22 @@ include "layout_header.php";
         <p>示例：</p>
 
         <pre><code class="language-html"><textarea>
-    <div class="x-alert x-alert-close">默认提示框</div>
-        <div class="x-alert x-alert-darkseagreen">Congratulation</div>
-        <div class="x-alert x-alert-cadetblue">信息框</div>
-        <div class="x-alert x-alert-indianred">引用框</div>
-        <div class="x-alert x-alert-darkorange">警告框</div>
-        <div class="x-alert x-alert-red">危险</div>
+    <div class="x-alert-base">默认提示框</div>
+        <div class="x-alert-darkseagreen">Congratulation</div>
+        <div class="x-alert-cadetblue">信息框</div>
+        <div class="x-alert-darkorange">警告框</div>
+        <div class="x-alert-red">危险</div>
 </textarea></code></pre>
 
 
         <p>效果:</p>
-        <div class="x-alert x-alert-close">默认提示框</div>
-        <div class="x-alert x-alert-darkseagreen">Congratulation</div>
-        <div class="x-alert x-alert-cadetblue">信息框</div>
-        <div class="x-alert x-alert-indianred">引用框</div>
-        <div class="x-alert x-alert-darkorange">警告框</div>
-        <div class="x-alert x-alert-red">危险</div>
+        <div class="x-alert-base">默认提示框</div>
+        <div class="x-alert-darkseagreen">Congratulation</div>
+        <div class="x-alert-cadetblue">信息框</div>
+        <div class="x-alert-darkorange">警告框</div>
+        <div class="x-alert-red">危险</div>
+
+        <div class="x-alert-quote">这是引用</div>
 
 
         <p>规则:</p>
