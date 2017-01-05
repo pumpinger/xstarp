e5ba2a3231c0af55cd8ab2a19fa7dac44b6a577a
gitjcc <jc_jesse@hotmail.com>
2017-01-05 18:18:24 +0800

    'timeformat'

---
+++ src/js/timeformat.js
@@ -0,0 +1,79 @@
+/**
+ * Created by Administrator on 2017/1/5.
+ */
+
+;(function (window,$) {
+    window.xTime = function (option) {
+        return new Time(option);
+    };
+    function Time(option) {
+        this._init(option);
+        return this;
+    }
+    Time.prototype = {
+        defaultOpt: {
+            wrap: '.x-progress-wrap',
+            pclass: 'x-progress-bar',
+            tclass: 'x-progress-only',
+            percentage: '2%',
+            onchange: function () {console.log('onchange')}
+        },
+        _init: function (option) {
+            this.option = $.extend(true, {}, this.defaultOpt, option);
+
+            if(typeof this.option.wrap === "string"){
+                this.wrap = $(this.option.wrap);
+            }else{
+                this.wrap = this.option.wrap;
+            }
+
+            if(typeof this.option.percentage === 'string'){
+                this._makeprogress();
+            }else{
+                // var that = this;
+                // $.each(this.percentage,function (index,item) {
+                //     that._makeprogress();
+                // });
+            }
+        },
+
+        _makeprogress: function () {
+
+            this.p = $('<div class="' + this.option.pclass + '" ></div>');
+            this.t = $('<span class="' + this.option.tclass + '" ></span>');
+            this.p.append(this.t);
+            this.percentage = this.option.percentage;
+
+            this.wrap.append(this.p);
+
+            this._observer(this,'percentage');
+            this._progress(this.percentage);
+        },
+
+        _observer: function (obj, k) {
+            var that = this;
+            var old = obj[k];
+            Object.defineProperty(obj, k, {
+                enumerable: true,
+                configurable: true,
+                get: function () {
+                    return old;
+                },
+                set: function (next) {
+                    if (next !== old) {
+                        that._progress(next, old);
+                        this.option.onchange();
+                    }
+                    old = next;
+                }
+            })
+        },
+        _progress: function (next, old) {
+            this.p.css({width: next});
+            this.t.text(next);
+        }
+    };
+
+    return Time;
+
+})(window,jQuery);
---
+++ timeformat.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/5
+ * Time: 18:06
+ */
+include "layout_header.php";
+?>
+
+
+
+
+
+<?php
+include "layout_footer.php";
+?>
