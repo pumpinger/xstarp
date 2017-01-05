2ca302c02687252722d6c5a2e65340652e263993
cherishjia <494976535@qq.com>
2017-01-05 10:32:26 +0800

    tip and  form

--- form.php
+++ form.php
@@ -19,108 +19,179 @@ include "layout_header.php";
     <div class="x-panel-title">标题：</div>
     <div class="x-panel-content">
 
-        <p>示例：</p>
+        <p class="x-color-cadetblue">示例：</p>
 
-<pre><code class="language-html"><textarea>
-
-
-</textarea></code></pre>
-
-
-        <p>效果:</p>
-        <div class="x-form">
+        <pre><code class="language-html"><textarea>
+   <div class="x-form">
             <h4 class="x-form-title">基本信息</h4>
             <form class="x-form-content">
                 <div class="x-form-item">
-                    <span class="x-form-key">姓名: </span>
+                    <span class="x-form-key">输入框: </span>
                     <span class="x-form-value"><input type="text" name="userName"></span>
                 </div>
-                <div  class="x-form-item">
+                <div class="x-form-item">
                     <span class="x-form-key">密码:</span>
                     <span class="x-form-value"><input type="password" name="passWord"></span>
                 </div>
-                <div  class="x-form-item">
-                    <span class="x-form-key">家庭住址:</span>
-                    <span class="x-form-value"><textarea></textarea></span>
+                <div class="x-form-item">
+                    <span class="x-form-key">文本框:</span>
+                    <span class="x-form-value"></span>
                 </div>
-                <div  class="x-form-item">
-                    <span class="x-form-key">性别:</span>
+                <div class="x-form-item">
+                    <span class="x-form-key">单选框:</span>
                     <span class="x-form-value">
                         <label class="x-form-input"><input name="radio" type="radio" value="1" checked="checked"><span>男</span></label>
                         <label class="x-form-input"><input name="radio" type="radio" value="2"><span>女</span></label>
                     </span>
                 </div>
-                <div  class="x-form-item">
-                    <span class="x-form-key">爱好:</span>
+                <div class="x-form-item">
+                    <span class="x-form-key">多选框:</span>
                     <span class="x-form-value">
                         <label class="x-form-input"><input name="checkbox" type="checkbox" value="1" checked="checked">1</label>
                         <label class="x-form-input"><input name="checkbox" type="checkbox" value="2">2</label>
                         <label class="x-form-input"><input name="checkbox" type="checkbox" value="3">3</label>
                     </span>
                 </div>
+                <div class="x-form-item">
+                    <span class="x-form-key">开关:</span>
+                    <span class="x-form-value">
+                        <label class="x-form-switch"><input type="checkbox"></label>
+                    </span>
+                </div>
+                <div class="x-form-item">
+                    <span class="x-form-key">品牌类型: </span>
+                    <div class="x-form-value">
+                        <lable class="x-form-radio x-checked">
+                            <input type="radio" name="radio1" value="选项1">选项1
+                        </lable>
+                        <lable class="x-form-radio">
+                            <input type="radio" name="radio1" value="选项2">选项2
+                        </lable>
+                    </div>
+                </div>
+                <div class="x-form-item">
+                    <span class="x-form-key">价格范围: </span>
+                    <div class="x-form-value">
+                        <label class="x-form-checkbox x-checked">
+                            <input type="checkbox" name="checkbox1" value="选项1">选项1
+                        </label>
+                        <label class="x-form-checkbox">
+                            <input type="checkbox" name="checkbox1" value="选项2">选项2
+                        </label>
+                        <label class="x-form-checkbox x-checked">
+                            <input type="checkbox" name="checkbox1" value="选项3">选项3
+                        </label>
 
+                    </div>
+                </div>
                 <div class="x-form-item">
                     <span class="x-form-key"></span>
                     <span class="x-form-value">
-                        <button class="x-form-submit">提交</button>
-                        <button class="x-form-cancel">取消</button>
+                        <a class="x-form-submit">提交</a>
+                        <a class="x-form-cancel">取消</a>
                     </span>
 
                 </div>
             </form>
         </div>
+</textarea></code></pre>
 
 
+        <p class="x-color-cadetblue">效果:</p>
         <div class="x-form">
-            <h4 class="x-form-title">表单选择</h4>
+            <h4 class="x-form-title">基本信息</h4>
             <form class="x-form-content">
+                <div class="x-form-item">
+                    <span class="x-form-key">输入框: </span>
+                    <div class="x-form-value"><input type="text" name="userName"></div>
+                </div>
+                <div  class="x-form-item">
+                    <span class="x-form-key">密码:</span>
+                    <div class="x-form-value"><input type="password" name="passWord"></div>
+                </div>
+                <div  class="x-form-item">
+                    <span class="x-form-key">文本框:</span>
+                    <div class="x-form-value"><textarea></textarea></div>
+                </div>
+                <div  class="x-form-item">
+                    <span class="x-form-key">单选框:</span>
+                    <div class="x-form-value">
+                        <label class="x-form-input"><input name="radio" type="radio" value="1" checked="checked"><span>男</span></label>
+                        <label class="x-form-input"><input name="radio" type="radio" value="2"><span>女</span></label>
+                    </div>
+                </div>
+                <div  class="x-form-item">
+                    <span class="x-form-key">多选框:</span>
+                    <div class="x-form-value">
+                        <label class="x-form-input"><input name="checkbox" type="checkbox" value="1" checked="checked">1</label>
+                        <label class="x-form-input"><input name="checkbox" type="checkbox" value="2">2</label>
+                        <label class="x-form-input"><input name="checkbox" type="checkbox" value="3">3</label>
+                    </div>
+                </div>
+                <div  class="x-form-item">
+                    <span class="x-form-key">开关:</span>
+                    <div class="x-form-value">
+                        <label class="x-form-switch"><input type="checkbox"></label>
+                    </div>
+                </div>
+                <div class="x-form-item">
+                    <span class="x-form-key">选择框:</span>
+                    <div class="x-form-value">
+                        <select class="x-form-select" name="select">
+                            <option value="1">普通用户</option>
+                            <option value="2">特殊用户</option>
+                            <option value="3">VIP</option>
+                        </select>
+                        <span>~</span>
+                        <select class="x-form-select" name="select">
+                            <option value="1">普通用户</option>
+                            <option value="2">特殊用户</option>
+                            <option value="3">VIP</option>
+                        </select>
+                    </div>
+                </div>
                 <div class="x-form-item">
                     <span class="x-form-key">品牌类型: </span>
                     <div class="x-form-value">
-                        <lable class="x-form-radio">
-                            <input type="radio" name="radio">选项1
+                        <lable class="x-form-radio x-checked">
+                            <input type="radio" name="radio1" value="选项1">选项1
                         </lable>
                         <lable class="x-form-radio">
-                            <input type="radio" name="radio">选项1
+                            <input type="radio" name="radio1" value="选项2">选项2
                         </lable>
                     </div>
                 </div>
-
-
                 <div class="x-form-item">
                     <span class="x-form-key">价格范围: </span>
                     <div class="x-form-value">
-                        <label class="x-form-checkbox">
-                            <input type="checkbox" name="checkbox">选项1
+                        <label class="x-form-checkbox x-checked">
+                            <input type="checkbox" name="checkbox1" value="选项1">选项1
                         </label>
                         <label class="x-form-checkbox">
-                            <input type="checkbox" name="checkbox">选项1
+                            <input type="checkbox" name="checkbox1" value="选项2">选项2
                         </label>
-                        <label class="x-form-checkbox">
-                            <input type="checkbox" name="checkbox">选项1
+                        <label class="x-form-checkbox x-checked">
+                            <input type="checkbox" name="checkbox1" value="选项3">选项3
                         </label>
 
                     </div>
+                </div>
+                <div class="x-form-item">
+                    <span class="x-form-key"></span>
+                    <div class="x-form-value">
+                        <a class="x-form-submit">提交</a>
+                        <a class="x-form-cancel">取消</a>
+                    </div>
+
                 </div>
             </form>
         </div>
 
 
-        <script>
-            $('.x-form-radio').click(function(){
-                $(this).addClass('x-checked').siblings().removeClass('x-checked');
-            })
-            $('.x-form-checkbox').click(function(){
-                if($(this).find("input").is(':checked')){
-                    $(this).addClass('x-checked');
-                }else{
-                    $(this).removeClass('x-checked')
-                }
 
-            })
-        </script>
-        <p>规则:</p>
 
+        <p class="x-color-cadetblue">规则:</p>
+        <p>1.命名形式 x-form- </p>
 
 
 
--- tip.php
+++ tip.php
@@ -18,42 +18,32 @@ include "layout_header.php";
         <p>示例：</p>
 
         <pre><code class="language-html"><textarea>
-
-</textarea></code></pre>
-
-
-        <p>效果:</p>
-            <div class="x-tip">
+<div class="x-tip">
                 <span class="x-tip-btn" data-tip="这是贴士内容">贴士层</span>
                 <div class="x-tip-dialog">
                     <div class="x-tip-content"></div>
                     <span class="x-tip-cor"></span>
                 </div>
             </div>
-
-        <script>
-
-            $('.x-tip-btn').mouseenter(function(){
-                $('.x-tip-content').text($(this).data("tip"));
-                $('.x-tip-dialog').show();
-                $('.x-tip-btn').mousemove(function(e){
-                    var mx = e.clientX-275;
-                    var my = e.clientY-280;
-
-                    $('.x-tip-dialog').css({display:'block',top:my,left:mx});
-                });
-            });
-            $('.x-tip-btn').mouseleave(function(){
-                $('.x-tip-dialog').hide();
-            });
+</textarea></code></pre>
 
 
-        </script>
+        <p>效果:</p>
+            <div class="x-tip">
+                <span class="x-tip-btn" data-title="这是贴士内容">贴士层</span>
+                <div class="x-tip-dialog">
+                    <div class="x-tip-content"></div>
+                    <span class="x-tip-cor"></span>
+                </div>
+            </div>
 
-
-
-
-
+        <div class="x-tip">
+            <span class="x-tip-btn" data-title="这是贴士内容" style="width:100px;white-space: nowrap;overflow:hidden;text-overflow:ellipsis;">贴士层贴士层贴士层贴士层</span>
+            <div class="x-tip-dialog">
+                <div class="x-tip-content"></div>
+                <span class="x-tip-cor"></span>
+            </div>
+        </div>
 
         <p>规则:</p>
     </div>
