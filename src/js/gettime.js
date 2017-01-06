7666a91207d059c875d2cd27b71de23a1fb3c147
Jesse <jc_jesse@hotmail.com>
2017-01-07 00:07:35 +0800

    'done.js'

--- src/js/gettime.js
+++ src/js/gettime.js
@@ -1,48 +1,111 @@
 /**
- * Created by Administrator on 2017/1/5.
+ * Created by Administrator on 2017/1/6.
  */
-
 ;(function () {
+    window.xTime = xtime();
 
-    window.xTime = gettime();
-
-    function gettime(date) {
+    function xtime() {
         var psecond = 1000,
             pminute = 60*psecond,
             phour = 60*pminute,
             pday = 24*phour,
-            pweek = 7*pday,
-            pmonth = 30*pday,
-            pyear = 365*pday;
+            pweek = 7*pday;
 
-        console.log('pday',pday,'pweek',pweek,'pmonth',pmonth,'pyear',pyear);
+        function gettime(date) {
+            var nowDate = date ? date : new Date();
 
-        var nowDate = new Date();
-        if(date){
-            nowDate = date;
-        }
+            var time = nowDate.getTime(),
+                msecond = nowDate.getMilliseconds(),
+                second = nowDate.getSeconds(),
+                minute = nowDate.getMinutes(),
+                hour = nowDate.getHours(),
+                dayWeek = nowDate.getDay(),
+                dayMonth = nowDate.getDate(),
+                month = nowDate.getMonth(),
+                year = nowDate.getFullYear();
 
-        var now = nowDate.getTime(),
-            ms = nowDate.getMilliseconds(),
-            second = nowDate.getSeconds(),
-            minute = nowDate.getMinutes(),
-            hour = nowDate.getHours(),
-            dw = nowDate.getDay(),
-            dm = nowDate.getDate(),
-            month = nowDate.getMonth(),
-            year = nowDate.getFullYear();
+            var dayPass = hour*phour + minute*pminute + second*psecond + msecond,
+                dayStart = time - dayPass,
+                dayEnd = dayStart + pday,
+                yesterdayStart = dayStart - pday,
+                pastDayStart = time - pday;
 
-        var dayP = hour*phour + minute*pminute+ second,
-            dayL = pday - dayP,
-            dayS = now - dayP,
-            dayE = now + dayL;
+            var weekPass = dayWeek * pday + dayPass,
+                weekStart = time - weekPass,
+                weekEnd = weekStart + pweek,
+                lastWeekStart = weekStart - pweek,
+                pastWeekStart = time - pweek;
+
+            var monthStart = (new Date(year, month, 1)).getTime(),
+                monthEnd = (new Date(year, month+1, 1)).getTime(),
+                lastMonthStart = (new Date(year, month-1, 1)).getTime(),
+                pastMonthStart = time - monthStart + lastMonthStart;
+
+            var yearStart = (new Date(year, 0, 1)).getTime(),
+                yearEnd = (new Date(year+1, 0, 1)).getTime(),
+                lastYearStart = (new Date(year-1, 0, 1)).getTime(),
+                pastYearStart = (new Date(time)).setFullYear(year-1);
 
 
-        return {
-            dayStart: function () {
-                return dayS;
+            return {
+                time: time,
+
+                day: {
+                    start: dayStart,
+                    end: dayEnd
+                },
+                yesterday: {
+                    start: yesterdayStart,
+                    end: dayStart
+                },
+                pastDay: {
+                    start: pastDayStart,
+                    end: time
+                },
+
+                week: {
+                    start: weekStart,
+                    end: weekEnd
+                },
+                lastWeek: {
+                    start: lastWeekStart,
+                    end: weekStart
+                },
+                pastWeek: {
+                    start: pastWeekStart,
+                    end: time
+                },
+
+                month: {
+                    start: monthStart,
+                    end: monthEnd
+                },
+                lastMonth: {
+                    start: lastMonthStart,
+                    end: monthStart
+                },
+                pastMonth: {
+                    start: pastMonthStart,
+                    end: time
+                },
+
+                year: {
+                    start: yearStart,
+                    end: yearEnd
+                },
+                lastYear: {
+                    start: lastYearStart,
+                    end: yearStart
+                },
+                pastYear: {
+                    start: pastYearStart,
+                    end: time
+                }
             }
         }
-    }
 
+        return {
+            time: gettime
+        };
+    }
 })();
