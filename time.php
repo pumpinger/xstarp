195a899f355007ff60dc4ddb1ec2b98989bb6186
DESKTOP-E5KLEFO\fizz <276161014@qq.com>
2017-01-06 18:39:57 +0800

    change datepicker

--- time.php
+++ time.php
@@ -7,115 +7,136 @@
  */
 include "layout_header.php";
 ?>
-<link rel="stylesheet" href="src/css/timepicker.css">
+<link rel="stylesheet" href="src/css/datepicker.css">
 
-<input type="text" id="timePicker">
+<h3>功能：</h3>
+<ul>
+    <li>选取日期</li>
+    <li>支持inline模式</li>
+    <li>支持IE8 & IE8+</li>
+</ul>
 
-<!--包裹的容器，多个picker的时候可以互斥-->
-<!--<div class="time-picker-container" id="time-picker-container">-->
-<!--    <div class="timepicker" >-->
-<!--        <div class="timepicker-nav">-->
-<!--            <div class="timepicker-nav-action action-left">-->
-<!--                <i class="icon iconfont icon-xiangzuo1"></i>-->
-<!--            </div>-->
-<!--            <div class="timepicker-nav-title">-->
-<!--                2016年12月-->
-<!--            </div>-->
-<!--            <div class="timepicker-nav-action action-right">-->
-<!--                <i class="icon iconfont icon-xiangyou1"></i>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="timepicker-content">-->
-<!--            <div class="timepicker-days" >-->
-<!--                <div class="timepicker-days-titles">-->
-<!--                    <div class="timepicker-days-title weekend">日</div>-->
-<!--                    <div class="timepicker-days-title">一</div>-->
-<!--                    <div class="timepicker-days-title">二</div>-->
-<!--                    <div class="timepicker-days-title">三</div>-->
-<!--                    <div class="timepicker-days-title">四</div>-->
-<!--                    <div class="timepicker-days-title">五</div>-->
-<!--                    <div class="timepicker-days-title weekend">六</div>-->
-<!--                </div>-->
-<!--                <div class="timepicker-days-cells">-->
-<!--                    <div class="timepicker-day-cell">1</div>-->
-<!--                    <div class="timepicker-day-cell">2</div>-->
-<!--                    <div class="timepicker-day-cell">3</div>-->
-<!--                    <div class="timepicker-day-cell">4</div>-->
-<!--                    <div class="timepicker-day-cell">5</div>-->
-<!--                    <div class="timepicker-day-cell">6</div>-->
-<!--                    <div class="timepicker-day-cell">7</div>-->
-<!--                    <div class="timepicker-day-cell">8</div>-->
-<!--                    <div class="timepicker-day-cell">9</div>-->
-<!--                    <div class="timepicker-day-cell">10</div>-->
-<!--                    <div class="timepicker-day-cell">11</div>-->
-<!--                    <div class="timepicker-day-cell">12</div>-->
-<!--                    <div class="timepicker-day-cell">13</div>-->
-<!--                    <div class="timepicker-day-cell">14</div>-->
-<!--                    <div class="timepicker-day-cell">15</div>-->
-<!--                    <div class="timepicker-day-cell">16</div>-->
-<!--                    <div class="timepicker-day-cell">17</div>-->
-<!--                    <div class="timepicker-day-cell">18</div>-->
-<!--                    <div class="timepicker-day-cell">19</div>-->
-<!--                    <div class="timepicker-day-cell">20</div>-->
-<!--                    <div class="timepicker-day-cell">21</div>-->
-<!--                    <div class="timepicker-day-cell">22</div>-->
-<!--                    <div class="timepicker-day-cell">23</div>-->
-<!--                    <div class="timepicker-day-cell">24</div>-->
-<!--                    <div class="timepicker-day-cell">25</div>-->
-<!--                    <div class="timepicker-day-cell">26</div>-->
-<!--                    <div class="timepicker-day-cell">27</div>-->
-<!--                    <div class="timepicker-day-cell">28</div>-->
-<!--                    <div class="timepicker-day-cell">29</div>-->
-<!--                    <div class="timepicker-day-cell">30</div>-->
-<!--                    <div class="timepicker-day-cell">31</div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="timepicker-months" style="display: none">-->
-<!--                <div class="timepicker-months-cells">-->
-<!--                    <div class="timepicker-month-cell">一月</div>-->
-<!--                    <div class="timepicker-month-cell">二月</div>-->
-<!--                    <div class="timepicker-month-cell">三月</div>-->
-<!--                    <div class="timepicker-month-cell">四月</div>-->
-<!--                    <div class="timepicker-month-cell">五月</div>-->
-<!--                    <div class="timepicker-month-cell">六月</div>-->
-<!--                    <div class="timepicker-month-cell">七月</div>-->
-<!--                    <div class="timepicker-month-cell">八月</div>-->
-<!--                    <div class="timepicker-month-cell">九月</div>-->
-<!--                    <div class="timepicker-month-cell">十月</div>-->
-<!--                    <div class="timepicker-month-cell">十一月</div>-->
-<!--                    <div class="timepicker-month-cell">十二月</div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="timepicker-years" style="display: none">-->
-<!--                <div class="timepicker-years-cells">-->
-<!--                    <!--                    <div class="timepicker-year-cell">1999</div>-->
-<!--                    <div class="timepicker-year-cell">2000</div>-->
-<!--                    <div class="timepicker-year-cell">2001</div>-->
-<!--                    <div class="timepicker-year-cell">2002</div>-->
-<!--                    <div class="timepicker-year-cell">2003</div>-->
-<!--                    <div class="timepicker-year-cell">2004</div>-->
-<!--                    <div class="timepicker-year-cell">2005</div>-->
-<!--                    <div class="timepicker-year-cell">2006</div>-->
-<!--                    <div class="timepicker-year-cell">2007</div>-->
-<!--                    <div class="timepicker-year-cell">2008</div>-->
-<!--                    <div class="timepicker-year-cell">2009</div>-->
-<!--                    <!--                    <div class="timepicker-year-cell">2010</div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
+<hr>
 
-<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
-<script src="src/js/timepicker.js"></script>
+<h3>如何使用：</h3>
+<pre>
+<code class="language-js">
+<textarea>
+引入：
+    <script src="//cdn.bootcss.com/jquery/1.11.0/jquery.min.js"></script>
+    <script src="src/js/datepicker.js"></script>
 
+在html标签里面添加插件标记：
+    <input data-toggle="datepicker">
+    <div data-toggle="datepicker"></div>
+
+初始化：
+    $('[data-toggle="datepicker"]').datepicker();
+
+注意：
+    支持IE8需使用jQuery 1.11.0版本（支持IE8的jQuery版本）。
+</textarea>
+</code>
+</pre>
+
+<h3>实例：</h3>
+<input type="text" id="timePicker" data-toggle="datepicker">
+
+<pre>
+<code class="language-js">
+<textarea>
+
+    <input type="text" id="timePicker" data-toggle="datepicker">
+
+    $('[data-toggle="datepicker"]').datepicker();
+</textarea>
+</code>
+</pre>
+
+<h3>方法：</h3>
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+
+<h3>options:</h3>
+
+<section>
+<h6>autoShow</h6>
+<p></p>
+</section>
+
+<section>
+    <h6>autoHide</h6>
+    <p></p>
+</section>
+
+<section>
+    <h6>autoPick</h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+<section>
+    <h6></h6>
+    <p></p>
+</section>
+
+
+
+
+
+<script src="//cdn.bootcss.com/jquery/1.11.0/jquery.min.js"></script>
+<script src="src/js/datepicker.js"></script>
 <script>
-    var datePicker = $('#timePicker').timePicker({startDate: 'new day'});
-    console.log(datePicker);
+    $('[data-toggle="datepicker"]').datepicker();
+    $('[data-toggle="datepicker"]').on('pick.datepicker', function (e) {
+        console.log('pick');
+        console.log(e);
+        if (e.date < new Date()) {
+            e.preventDefault(); // Prevent to pick the date
+        }
+    });
 </script>
 
 
-<?php
 
+
+<?php
 include "layout_footer.php";
 ?>
