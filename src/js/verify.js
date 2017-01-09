3c5dad888f343a5e772fdf60fd45d28c5f3fea12
gitjcc <jc_jesse@hotmail.com>
2017-01-09 12:20:37 +0800

    'verify.js'

---
+++ src/js/verify.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/1/9.
+ */
+;(function () {
+    window.xVerify = verify;
+    function verify() {
+        
+    }
+})();
---
+++ verify.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/9
+ * Time: 11:25
+ */
+include 'layout_header.php';
+?>
+
+
+
+
+
+
+<?php
+include 'layout_footer.php';
+?>
