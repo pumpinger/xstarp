f0adf4e3a07f3c8b70f83d46df561c7c687922fc
gitjcc <jc_jesse@hotmail.com>
2017-01-17 12:30:59 +0800

    'time.php-master'

--- time.php
+++ time.php
@@ -10,7 +10,7 @@ include "layout_header.php";
 
 
 <h3>示例请看此页：<a style="color:blue;" href="https://rawgit.com/longbill/jquery-date-range-picker/master/index.html">示例</a></h3>
-<h3>中文文档：<a href="https://github.com/xbcx/xstarp/blob/fizz/docs/datepicker.md" style="color:blue;">文档</a></h3>
+<h3>中文文档：<a href="https://github.com/xbcx/xstarp/blob/master/docs/datepicker.md" style="color:blue;">文档</a></h3>
 <?php
 include "layout_footer.php";
 ?>
