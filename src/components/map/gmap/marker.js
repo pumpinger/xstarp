a6f1ccc57f667caee84d342b2360464bf46a2bf7
DESKTOP-E5KLEFO\fizz <276161014@qq.com>
2017-02-13 18:45:36 +0800

    map

---
+++ src/components/map/gmap/marker.js
@@ -0,0 +1,22 @@
+/**
+ * Created by fizz on 2017/2/13.
+ */
+
+/**
+ * @constructor lngLat
+ * @lng {Number} lng
+ * @lat {Number} lat
+ * */
+var LngLat = function(lng, lat) {
+  this.lat = lat;
+  this.lng = lng;
+};
+
+Map.prototype = {
+
+};
+
+module.exports = Map;
+
+
+
