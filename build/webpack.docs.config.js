dee3af4c40fe1a0f217652f376a1f5130a1f2220
fizz stack <276161014@qq.com>
2017-02-23 00:39:47 +0800

    自动复制dist的vendor们到docs的dist文件夹
    自动复制dist的vendor们到docs的dist文件夹

--- build/webpack.docs.config.js
+++ build/webpack.docs.config.js
@@ -1,10 +1,17 @@
 var config = require('./webpack.prod.config');
 var path = require('path');
 var opn = require('opn');
+var copyDir = require('copy-dir');
+var from = path.resolve(__dirname, '../dist');
+var to   = path.resolve(__dirname, '../docs/dist');
 
 config.watch = true;
 config.output.path = path.resolve(__dirname,'../docs/dist');
 config.output.publicPath = path.resolve(__dirname,'../docs/dist/');
+
+
+
+copyDir.sync(from, to);
 
 opn('http://localhost:9100');
 
