9c04fed4c07e4e1fb0183665a793f840e5d1f166
Unremittingly <406121138@qq.com>
2017-02-24 11:21:34 +0800

    zq  -upload

--- src/components/upload/upload.js
+++ src/components/upload/upload.js
@@ -42,101 +42,10 @@ var WebUploader = require('./webuploader.js');
     // 所有文件的进度信息，key为file id
         percentages = {},
     // 判断浏览器是否支持图片的base64
-        isSupportBase64 = ( function() {
-            var data = new Image();
-            var support = true;
-            data.onload = data.onerror = function() {
-                if( this.width != 1 || this.height != 1 ) {
-                    support = false;
-                }
-            };
-            data.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-            return support;
-        } )(),
-
-    //检测是否已经安装flash，检测flash的版本
-        flashVersion = ( function() {
-            var version;
-
-            try {
-                version = navigator.plugins[ 'Shockwave Flash' ];
-                version = version.description;
-            } catch ( ex ) {
-                try {
-                    version = new ActiveXObject('ShockwaveFlash.ShockwaveFlash')
-                        .GetVariable('$version');
-                } catch ( ex2 ) {
-                    version = '0.0';
-                }
-            }
-            version = version.match( /\d+/g );
-            return parseFloat( version[ 0 ] + '.' + version[ 1 ], 10 );
-        } )(),
-
-        supportTransition = (function(){
-            var s = document.createElement('p').style,
-                r = 'transition' in s ||
-                    'WebkitTransition' in s ||
-                    'MozTransition' in s ||
-                    'msTransition' in s ||
-                    'OTransition' in s;
-            s = null;
-            return r;
-        })(),
 
     // WebUploader实例
         uploader;
-    if ( !WebUploader.Uploader.support('flash') && WebUploader.browser.ie ) {
 
-        // flash 安装了但是版本过低。
-        if (flashVersion) {
-            (function(container) {
-                window['expressinstallcallback'] = function( state ) {
-                    switch(state) {
-                        case 'Download.Cancelled':
-                            alert('您取消了更新！');
-                            break;
-
-                        case 'Download.Failed':
-                            alert('安装失败');
-                            break;
-
-                        default:
-                            alert('安装已成功，请刷新！');
-                            break;
-                    }
-                    delete window['expressinstallcallback'];
-                };
-
-                var swf = './expressInstall.swf';
-                // insert flash object
-                var html = '<object type="application/' +
-                    'x-shockwave-flash" data="' +  swf + '" ';
-
-                if (WebUploader.browser.ie) {
-                    html += 'classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ';
-                }
-
-                html += 'width="100%" height="100%" style="outline:0">'  +
-                    '<param name="movie" value="' + swf + '" />' +
-                    '<param name="wmode" value="transparent" />' +
-                    '<param name="allowscriptaccess" value="always" />' +
-                    '</object>';
-
-                container.html(html);
-
-            })($wrap);
-
-            // 压根就没有安转。
-        } else {
-            $wrap.html('<a href="http://www.adobe.com/go/getflashplayer" target="_blank" border="0"><img alt="get flash player" src="http://www.adobe.com/macromedia/style_guide/images/160x41_Get_Flash_Player.jpg" /></a>');
-        }
-
-        return;
-    } else if (!WebUploader.Uploader.support()) {
-        alert( 'Web Uploader 不支持您的浏览器！');
-        return;
-    }
     //图片上传和文件上传区分 参数
     var opt_file = {
         dom:'',
@@ -171,18 +80,112 @@ var WebUploader = require('./webuploader.js');
     var defaultOption = opt_file;
     var upload=function(opt,dom){
         this.opt = $.extend(true,{},defaultOption,opt);
-        this.init(this.opt);
-
+        if(this.isFlash){
+            this.init(this.opt);
+        }
         return this;
     };
 
     upload.prototype ={
+        isSupportBase64:( function() {
+            var data = new Image();
+            var support = true;
+            data.onload = data.onerror = function() {
+                if( this.width != 1 || this.height != 1 ) {
+                    support = false;
+                }
+            };
+            data.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+            return support;
+        } )(),
+        //检测是否已经安装flash，检测flash的版本
+        flashVersion : ( function() {
+            var version;
+            try {
+                version = navigator.plugins[ 'Shockwave Flash' ];
+                version = version.description;
+            } catch ( ex ) {
+                try {
+                    version = new ActiveXObject('ShockwaveFlash.ShockwaveFlash')
+                        .GetVariable('$version');
+                } catch ( ex2 ) {
+                    version = '0.0';
+                }
+            }
+            version = version.match( /\d+/g );
+            return parseFloat( version[ 0 ] + '.' + version[ 1 ], 10 );
+        } )(),
+
+        supportTransition : (function(){
+            var s = document.createElement('p').style,
+                r = 'transition' in s ||
+                    'WebkitTransition' in s ||
+                    'MozTransition' in s ||
+                    'msTransition' in s ||
+                    'OTransition' in s;
+            s = null;
+            return r;
+        })(),
+        isFlash:function(){
+            if ( !WebUploader.Uploader.support('flash') && WebUploader.browser.ie ) {
+
+                // flash 安装了但是版本过低。
+                if (this.flashVersion) {
+                    (function(container) {
+                        window['expressinstallcallback'] = function( state ) {
+                            switch(state) {
+                                case 'Download.Cancelled':
+                                    alert('您取消了更新！');
+                                    break;
+
+                                case 'Download.Failed':
+                                    alert('安装失败');
+                                    break;
+
+                                default:
+                                    alert('安装已成功，请刷新！');
+                                    break;
+                            }
+                            delete window['expressinstallcallback'];
+                        };
+
+                        var swf = './expressInstall.swf';
+                        // insert flash object
+                        var html = '<object type="application/' +
+                            'x-shockwave-flash" data="' +  swf + '" ';
+
+                        if (WebUploader.browser.ie) {
+                            html += 'classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ';
+                        }
+
+                        html += 'width="100%" height="100%" style="outline:0">'  +
+                            '<param name="movie" value="' + swf + '" />' +
+                            '<param name="wmode" value="transparent" />' +
+                            '<param name="allowscriptaccess" value="always" />' +
+                            '</object>';
+
+                        container.html(html);
+
+                    })($wrap);
+
+                    // 压根就没有安转。
+                } else {
+                    $wrap.html('<a href="http://www.adobe.com/go/getflashplayer" target="_blank" border="0"><img alt="get flash player" src="http://www.adobe.com/macromedia/style_guide/images/160x41_Get_Flash_Player.jpg" /></a>');
+                }
+
+                return;
+            } else if (!WebUploader.Uploader.support()) {
+                alert( 'Web Uploader 不支持您的浏览器！');
+                return;
+            }
+        },
         init:function(opt){
+
             var fileType = opt.fileType;
             var addButton = opt.addButton;
 
             opt.pick = opt.fileButton;
-
+            var _that = this;
             if(fileType == 1){
                 opt['accept'] = {
                     title: 'Images',
@@ -382,7 +385,7 @@ var WebUploader = require('./webuploader.js');
                             $wrap.text( '不能预览' );
                             return;
                         }
-                        if( isSupportBase64 ) {
+                        if( _that.isSupportBase64 ) {
                             img = $('<img src="'+src+'">');
                             $wrap.empty().append( img );
                         } else {
@@ -652,7 +655,7 @@ var WebUploader = require('./webuploader.js');
                             //$wrap.text( '不能预览' );
                             //return;
                         }
-                        if (isSupportBase64) {
+                        if (_that.isSupportBase64) {
                             img = $('<img src="' + src + '"><span class="x-fileName">' + file.name + '</span>');
                             $wrap.find(".x-imgFile").append(img);
                         } else {
