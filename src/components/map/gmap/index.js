a57313ee714817294b5d1a3d5869f8ae6ece6881
gitjcc <jc_jesse@hotmail.com>
2017-03-28 19:32:47 +0800

    gmap : InfoWindow  setPosition

--- src/components/map/gmap/index.js
+++ src/components/map/gmap/index.js
@@ -8,7 +8,7 @@ var Pixel = require('./Pixel');
 var Size = require('./Size');
 var Map = require('./Map');
 var Marker = require('./Marker');
-var lMarker = require('./LMarker');
+var LMarker = require('./LMarker');
 var event = require('./Event');
 var InfoWindow = require('./InfoWindow');
 var Polyline = require('./Polyline');
@@ -27,7 +27,7 @@ GMap.Size = Size;
 
 GMap.Map = Map;
 GMap.Marker = Marker;
-GMap.lMarker = lMarker;
+GMap.LMarker = LMarker;
 GMap.InfoWindow = InfoWindow;
 GMap.Polyline = Polyline;
 GMap.Polygon = Polygon;
