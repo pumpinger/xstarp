e6c47d0d007a37c0eb4038812b4cf4354aa903c3
gitjcc <jc_jesse@hotmail.com>
2017-03-30 15:18:25 +0800

    refactor : gmap Pixel.js

--- src/components/map/gmap/Pixel.js
+++ src/components/map/gmap/Pixel.js
@@ -5,44 +5,40 @@
  * https://developers.google.com/maps/documentation/javascript/reference#Point
  */
 
-
 /**
  * @constructor
  * @param {Number} x
  * @param {Number} y
  * */
-function Pixel(x, y, inner) {
-  if(inner) {
-    this._inner = inner;
-  } else {
-    this._inner = new google.maps.Point(x,y);
-  }
+var format = require('./util/format.js');
 
-  this._type = 'Pixel';
-  return this;
+function Pixel(x, y, inner) {
+    if (inner) {
+        this._inner = inner;
+    } else {
+        this._inner = new google.maps.Point(x, y);
+    }
+    this._type = 'Pixel';
+    return this;
 }
 
-Pixel.prototype = {
-  getX: function() {
+Pixel.prototype.getX = function () {
     return this._inner.x;
-  },
-  getY: function() {
+};
+Pixel.prototype.getY = function () {
     return this._inner.y;
-  },
-
-  /**
-   * @param {Pixel} point
-   * */
-  equals: function(point) {
-    return this._inner.equals(point);
-  },
-
-  toString: function() {
-    return this._inner.toString();
-  }
 };
 
+/**
+ * @param {Pixel} pixel
+ * */
+Pixel.prototype.equals = function (pixel) {
+    var point = format({Point: pixel}).Point;
+    return this._inner.equals(point);
+};
 
-
+Pixel.prototype.toString = function () {
+    return this._inner.toString();
+};
 
 module.exports = Pixel;
