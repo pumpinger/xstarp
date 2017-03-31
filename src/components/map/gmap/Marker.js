bfa86cfd4c0507cbb45907e27a7a560f77a95a00
gitjcc <jc_jesse@hotmail.com>
2017-03-31 19:47:44 +0800

    fix : gmap Marker getPosition() hide() show()

--- src/components/map/gmap/Marker.js
+++ src/components/map/gmap/Marker.js
@@ -4,7 +4,7 @@
  */
 var obc = require('./util/overlayBaseClass');
 var onOff = require('./util/onOff');
-var formatOpts = require('./util/formatOpt');
+var format = require('./util/format.js');
 var LMarker = require('./LMarker');
 
 /**
@@ -15,36 +15,29 @@ var LMarker = require('./LMarker');
  * @return an object, inner is prime google map Marker instance.
  * */
 function Marker(opts, inner) {
-
-    this._type = 'Marker';
-
     if (inner) {
         this._inner = inner;
     } else {
         // 在opts转换之前就要判断添加overlay
         // obc.addOverlay(opts, this);
-        var newOpts = formatOpts.marker(opts);
-
+        var newOpts = format.marker(opts);
         if (newOpts.content) {
             newOpts.icon = 'http://c163img.nos-eastchina1.126.net/blank_36x36.png';
-            // newOpts.icon = 'http://c163img.nos-eastchina1.126.net/blank_36x36.png';
             newOpts.label = newOpts.content;
             this._inner = new LMarker(newOpts);
+            console.log('LMarker',this._inner);
+            
         } else {
             this._inner = new google.maps.Marker(newOpts);
         }
-
         this._inner._smap = newOpts.map;
         this.position = newOpts.position;
-
-        google.maps.Marker.apply(this, opts);
-        console.log('this', this);
-        console.log('this._inner', this._inner);
-        //marker2._inner.__gm.Eb.map.b.O.style.opacity = 1
     }
+    this._type = 'Marker';
+    return this;
 }
 
-Marker.prototype = new google.maps.Marker(new google.maps.LatLng(0, 0));
+// Marker.prototype = new google.maps.Marker();
 
 Marker.prototype.setMap = function (map) {
     if (map !== null) {
@@ -53,14 +46,12 @@ Marker.prototype.setMap = function (map) {
         this._inner.setMap(map._inner);
         var that = this;
         console.log('inner',that._inner.__gm);
-        setTimeout(
-            function () {
-                if (that._inner.content && that._inner.__gm.Eb.map) {
-                    that._inner.__gm.Eb.map.b.O.style.opacity = 1;
-                    that._inner.__gm.Eb.map.b.O.innerHTML = that._inner.content;
-                }
-            }, 1000
-        );
+        setTimeout(function () {
+            if (that._inner.content && that._inner.__gm.Eb.map) {
+                that._inner.__gm.Eb.map.b.O.style.opacity = 1;
+                that._inner.__gm.Eb.map.b.O.innerHTML = that._inner.content;
+            }},
+        1000);
 
     } else {
         this._inner.setMap(null);
@@ -68,14 +59,14 @@ Marker.prototype.setMap = function (map) {
 };
 Marker.prototype.getMap = obc.getMap;
 Marker.prototype.getPosition = function () {
-    return this.position;
+    return this._inner.getPosition();
 };
 
 Marker.prototype.hide = function () {
-    this._inner.hide()
+    this._inner.setVisible(false);
 };
 Marker.prototype.show = function () {
-    this._inner.show()
+    this._inner.setVisible(true);
 };
 Marker.prototype.on = onOff.on;
 Marker.prototype.off = onOff.off;
